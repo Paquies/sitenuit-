3e54fb4092a8d2f0f1ae9fe5f25dcb875a4485df
ShowYL <130548077+ShowYL@users.noreply.github.com>
2023-12-08 00:52:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-function validateForm() {
-    var nom = document.forms["myForm"]["nom"].value;
-    var mdp = document.forms["myForm"]["mdp"].value;
-    var atPosition = nom.indexOf("@");
-    if (atPosition < 5 || !nom.endsWith("@nuit-info.tlse") || mdp != "12345") {
-        alert("Nom d'utilisateur ou mot de passe incorrect");
-        return false;
-    } else {
-        window.location.href = "page_presentation.html"; // Redirige vers une nouvelle page
-        return false;// Pour empêcher le rechargement de la page
+window.onload = function() {
+    var registerForm = document.getElementById("registerForm");
+    var loginForm = document.getElementById("loginForm");
+
+    if(registerForm) {
+        registerForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            
+            var nom = document.getElementById("nom").value;
+            var mdp = document.getElementById("mdp").value;
+            
+            var users = JSON.parse(localStorage.getItem('users')) || [];
+            
+            users.push({nom: nom, mdp: mdp});
+            
+            localStorage.setItem('users', JSON.stringify(users));
+            
+            alert("Inscription réussie !");
+        });
+    }
+
+    if(loginForm) {
+        loginForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            
+            var nom = document.getElementById("nom").value;
+            var mdp = document.getElementById("mdp").value;
+            
+            var users = JSON.parse(localStorage.getItem('users')) || [];
+            
+            for(var i = 0; i < users.length; i++) {
+                if(users[i].nom == nom && users[i].mdp == mdp) {
+                    window.location.href = "page_presentation.html";
+                    return;
+                }
+            }
+            
+            alert("Nom d'utilisateur ou mot de passe incorrect");
+        });
     }
 }
